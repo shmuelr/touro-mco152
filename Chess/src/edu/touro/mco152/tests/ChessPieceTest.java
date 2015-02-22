ab1dc79bafae37bd70aae1757b9ac613c956d853
Shmuel Rosansky <sdrosansky@gmail.com>
2015-02-22 14:53:16 -0500

    ChessPiece tests

---
+++ Chess/src/edu/touro/mco152/tests/ChessPieceTest.java
@@ -0,0 +1,14 @@
+package edu.touro.mco152.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ChessPieceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
