4cec103d67693c19867ce98439d5e09fc33b55b8
Shmuel Rosansky <sdrosansky@gmail.com>
2015-02-22 14:09:21 -0500

    Add method to get position of a piece

--- Chess/src/edu/touro/mco152/ChessPiece.java
+++ Chess/src/edu/touro/mco152/ChessPiece.java
@@ -1,10 +1,14 @@
 package edu.touro.mco152;
 
+import java.awt.Point;
+
 public interface ChessPiece {
 	public static enum PieceColor {
 		BLACK, WHITE
 	}
 	
 	public PieceColor getColor();
+	
+	public Point getPosition();
 
 }
