04c85352e098cb33a2c6ba7d07dc38a0d477437b
Shmuel Rosansky <sdrosansky@gmail.com>
2015-02-25 10:43:50 -0500

    Add test suite for pawn and board tests

---
+++ Chess/src/edu/touro/mco152/tests/AllTests.java
@@ -0,0 +1,11 @@
+package edu.touro.mco152.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ BoardTest.class, PawnTest.class })
+public class AllTests {
+
+}
